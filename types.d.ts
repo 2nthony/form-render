0c2ee1733f869713dc3574cd3b38ab3dbde41626
evillt <ijoec123@gmail.com>
2019-03-24 01:37:34 +0800

    chore: update types

--- types.d.ts
+++ types.d.ts
@@ -1,15 +1,49 @@
-export interface Props {
+declare type Props = {
   [prop: string]: any
 }
 
-export interface Item {
-  model: string
+declare type Options = {
+  value: any
   label?: string
+}
+
+declare type Item = {
+  /**
+   * Value model
+   */
+  model: string
+  /**
+   * Form item label
+   */
+  label?: string
+  /**
+   * Mapping component
+   *
+   * e.g.
+   * `type: 'text'` = `<input>`
+   */
   type?: string
+  /**
+   * Form item default value
+   */
+  value?: any
+  /**
+   * Form item props options
+   */
+  props?: Props
+  /**
+   * Option type component's options
+   *
+   * e.g.
+   * `select` `radio` `checkbox` ...
+   */
+  options?: Options[]
 }
 
 export interface Config {
-  type: string
   props?: Props
+  /**
+   * To render form items
+   */
   items?: Item[]
 }
